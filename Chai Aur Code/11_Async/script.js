564df01dc5d22b71fbf91d7264cebc00717fed0b
Nihar Patel <patelnihar14@gmail.com>
2024-01-20 17:02:41 +0530

    .

--- Chai Aur Code/11_Async/script.js
+++ Chai Aur Code/11_Async/script.js
@@ -1,7 +1,9 @@
-console.log(1);
+// console.log(1);
 
-setTimeout(() => {
-  console.log(2);
-}, 0);
+// setTimeout(() => {
+//   console.log(2);
+// }, 0);
 
-console.log(3);
+// console.log(3);
+
+// -------------------------
