dd9ee2ca7faee68b18898c59ddae6b79436375c8
Nihar Patel <patelnihar14@gmail.com>
2024-03-28 19:49:46 +0530

    .

--- Revised3/script.js
+++ Revised3/script.js
@@ -1,1 +1,23 @@
- 
+// let firstName = "Nihar";
+// let lastName = "Patel";
+// let age = 24;
+// let isHappy = true;
+
+// let userIntro =
+//   "Hi my name is " +
+//   firstName +
+//   " " +
+//   lastName +
+//   " " +
+//   "i am " +
+//   " " +
+//   age +
+//   " year old.";
+
+// console.log("userIntro:", userIntro);
+
+// STRING METHOD...........
+
+let userName = "Nihar";
+
+console.log(userName[0]);
