727f9728b4206c855730d17ab8769d5e8f158947
Nihar Patel <patelnihar14@gmail.com>
2024-05-16 13:15:13 +0530

    day 48 done

--- 100_Que/script.js
+++ 100_Que/script.js
@@ -928,14 +928,25 @@
 
 //================================= Day-47 ==================================================
 
-function extractNumbers(str) {
-  const regex = /\d+/g;
+// function extractNumbers(str) {
+//   const regex = /\d+/g;
 
-  return str.match(regex);
-}
+//   return str.match(regex);
+// }
 
-console.log(extractNumbers('abc123de456'));
-console.log(extractNumbers('no number '));
-console.log(extractNumbers('1a2b3c4d'));
-console.log(extractNumbers('100,200 and 300'));
-console.log(extractNumbers(''));
+// console.log(extractNumbers('abc123de456'));
+// console.log(extractNumbers('no number '));
+// console.log(extractNumbers('1a2b3c4d'));
+// console.log(extractNumbers('100,200 and 300'));
+// console.log(extractNumbers(''));
+
+//================================= Day-48 ==================================================
+
+const validateHexColor = (str) => {
+  return /^#([A-Fa-f\d]{3}|[A-Fa-f\d]{6})$/.test(str);
+};
+
+console.log(validateHexColor('#a3c113'));
+console.log(validateHexColor('#fff'));
+console.log(validateHexColor('#1234567'));
+console.log(validateHexColor('#g3c113'));
